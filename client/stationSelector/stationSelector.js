a9485a7fce52a43d7ea0d269a6fdb7da8e6cfee1
Eduardo Medina <medina.eag@gmail.com>
2016-10-28 23:48:53 -0700

    Started arranging sessions

--- client/stationSelector/stationSelector.js
+++ client/stationSelector/stationSelector.js
@@ -4,10 +4,22 @@ import './stationSelector.html';
 
 Template.stationSelector.onCreated(function() {
   this.subscribe('trainStationNames');
+  Session.set("departure", "San Francisco Caltrain");
+  Session.set("arrival", "San Francisco Caltrain");
 });
 
 Template.stationSelector.helpers({
 	trainStationNames() {
 		return Stops.find({});
+	}
+});
+
+Template.stationSelector.events({
+	'change #departure'(event){
+		console.log(typeof(event.target.value));
+		Session.set("departure", event.target.value);
+	},
+	'change #arrival'(event){
+		console.log(event.target.value);
 	}
 });
