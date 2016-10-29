ae164a3e99e01697b854dd662aedbb4465b4de66
Eduardo Medina <medina.eag@gmail.com>
2016-10-28 21:25:59 -0700

    Added backend to populate DB and establish a publication

--- server/main.js
+++ server/main.js
@@ -1,5 +1,35 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  if (Stops.find().count() === 0) {
+  	console.log("Stops collection is empty. Populating...");
+  	var stopsJSON = {}
+  	stopsJSON = JSON.parse(Assets.getText("stops.json"));
+  	for (var doc in stopsJSON ) {
+  		Stops.insert(stopsJSON[doc]);
+  	}
+  }
+
+  if (Trips.find().count() === 0) {
+  	console.log("Trips collection is empty. Populating...");
+  	var tripsJSON = {}
+  	tripsJSON = JSON.parse(Assets.getText("trips.json"));
+  	for (var doc in tripsJSON ) {
+  		Trips.insert(tripsJSON[doc]);
+  	}
+  }
+
+  if (Times.find().count() === 0) {
+  	console.log("Times collection is empty. Populating...");
+  	var timesJSON = {}
+  	timesJSON = JSON.parse(Assets.getText("times.json"));
+  	for (var doc in timesJSON ) {
+  		Times.insert(timesJSON[doc]);
+  	}
+  }
+});
+
+Meteor.publish('trainStationNames', function() {
+	var stations = Stops.find({"platform_code": "SB"}, {fields: {"stop_name": 1}});
+	return stations
 });
