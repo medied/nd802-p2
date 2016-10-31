1137a46469060d2aaef40cd9e82732f0449ace56
Eduardo Medina <medina.eag@gmail.com>
2016-10-30 20:55:16 -0700

    Minor updates to stationSelector.js

--- client/stationSelector/stationSelector.js
+++ client/stationSelector/stationSelector.js
@@ -3,9 +3,9 @@ import { Template } from 'meteor/templating';
 import './stationSelector.html';
 
 Template.stationSelector.onCreated(function() {
-  this.subscribe('trainStationNames');
-  Session.set("departure", "San Francisco Caltrain");
-  Session.set("arrival", "San Francisco Caltrain");
+  this.subscribe('stationNames');
+  Session.set("departure", "");
+  Session.set("arrival", "");
 });
 
 Template.stationSelector.helpers({
@@ -16,10 +16,11 @@ Template.stationSelector.helpers({
 
 Template.stationSelector.events({
 	'change #departure'(event){
-		console.log(typeof(event.target.value));
 		Session.set("departure", event.target.value);
+		console.log("Departure session var set to: ", event.target.value);
 	},
 	'change #arrival'(event){
-		console.log(event.target.value);
+		Session.set("arrival", event.target.value);
+		console.log("Arrival session var set to: ", event.target.value);
 	}
 });
