7c555c9b743a43af01da4de9ccda4e00386de9d1
Eduardo Medina <medina.eag@gmail.com>
2016-11-14 22:12:41 -0800

    Fixed issue #5

--- client/main.js
+++ client/main.js
@@ -2,11 +2,24 @@ import { Template } from 'meteor/templating';
 
 import './main.html';
 
+Template.body.onCreated(function () {
+	Session.set("sameStation", false);
+});
+
 Template.body.helpers({
 	tripSelected() {
-		if ((Session.get("departure") != "") && (Session.get("arrival") != "")) {
+		if (Session.get("departure") === Session.get("arrival")){
+			Session.set("sameStation", true);
+			Session.set("tripSelected", false);
+			return false;
+		} else if ((Session.get("departure") != "") && (Session.get("arrival") != "")) {
 			Session.set("tripSelected", true);
+			Session.set("sameStation", false);
 			return true;
 		}
+	},
+	sameStation() {
+		if (Session.get("sameStation") === true)
+		return true;
 	}
 });
